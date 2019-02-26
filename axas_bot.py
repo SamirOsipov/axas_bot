b7fe587e2427f845bcf1607bb94c01f73487a1a0
Самир Осипов <seriousosa@gmail.com>
2019-02-26 14:04:41 +0300

    Add welcomes

--- axas_bot.py
+++ axas_bot.py
@@ -26,4 +26,22 @@ def sticker_handler(message: Message):
     bot.send_sticker(message.chat.id, STICKER_ID)
 
 
+@bot.message_handler(commands=['app'])
+def choose_buttom(message: Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    callback_button = types.KeyboardButton(text='Android-приложение')
+    keyboard.row(u'\uD83C\uDD95' + ' Android-приложение', u'\uD83D\uDC6B' + ' iOS-приложение')
+    keyboard.row('Назад')
+
+    bot.send_message(message.chat.id, '''<b>Приложение</b>  ''', reply_markup=keyboard,parse_mode='HTML')
+
+@bot.message_handler(commands=["phone"])
+def geophone(message):
+    # Эти параметры для клавиатуры необязательны, просто для удобства
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
+    keyboard.row(button_phone)
+    bot.send_message(message.chat.id, "Оставьте совой номер телефона!", reply_markup=keyboard)
+
+
 bot.polling(timeout=60)
