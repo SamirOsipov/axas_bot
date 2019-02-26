009fe256a24bfe7f40dd49bb46110f97251d9d94
Самир Осипов <seriousosa@gmail.com>
2019-02-26 16:27:16 +0300

    add app

--- axas_bot.py
+++ axas_bot.py
@@ -11,13 +11,30 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def command_handler(message: Message):
-    #клавиатура для бота
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_tell = types.KeyboardButton(text='Рассказать о нас')
-    keyboard.add(button_tell)
+    # инлайнклавиатура для бота
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Подробнее о нас", url="http://axas-soft.ru")
+    keyboard.add(url_button)
+    # клавиатура для бота
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row('Мобильные приложения', 'Веб-платформы')
+    keyboard.row('Нейросети', 'Связаться с нами')
     bot.send_message(message.chat.id, '''<b>Добро пожаловать!</b> 
-    
-Привет! Это компания AXAS''', reply_markup=keyboard, parse_mode='HTML')
+Это телеграм бот компании AXAS. Что Вас интересует?''', reply_markup=keyboard, parse_mode='HTML')
+
+
+@bot.message_handler(commands=['contacts'])
+def default_test(message):
+    keyboard = types.InlineKeyboardMarkup()
+    site_button = types.InlineKeyboardButton(text="Вебсайт", url="http://axas-soft.ru/")
+    vk_button = types.InlineKeyboardButton(text="Вконтакте", url="https://vk.com/axas_soft")
+    inst_button = types.InlineKeyboardButton(text="Инстаграм", url="http://axas-soft.ru/")
+    fb_button = types.InlineKeyboardButton(text="Фейсбук", url="https://www.facebook.com/iAXAS/")
+    keyboard.add(site_button)
+    keyboard.add(vk_button)
+    keyboard.add(fb_button)
+    keyboard.add(inst_button)
+    bot.send_message(message.chat.id, "Hаши контакты.", reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=['sticker'])
@@ -26,14 +43,24 @@ def sticker_handler(message: Message):
     bot.send_sticker(message.chat.id, STICKER_ID)
 
 
+@bot.message_handler(content_types=['text'])
+def app(message: Message):
+    if ('Мобильные приложения' in message.text):
+        keyboard = types.InlineKeyboardMarkup()
+        button_1 = types.InlineKeyboardButton(text='Приложение для бизнеса', url="http://axas-soft.ru")
+        button_2 = types.InlineKeyboardButton(text='Игры', url="http://axas-soft.ru")
+        keyboard.add(button_1, button_2)
+        bot.send_message(message.chat.id, 'Какую категорию приложения вы хотите заказать', reply_markup=keyboard)
+
+
 @bot.message_handler(commands=['app'])
 def choose_buttom(message: Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     callback_button = types.KeyboardButton(text='Android-приложение')
     keyboard.row(u'\uD83C\uDD95' + ' Android-приложение', u'\uD83D\uDC6B' + ' iOS-приложение')
-    keyboard.row('Назад')
+    keyboard.row('Обе платформы')
+    bot.send_message(message.chat.id, '''<b>Выберите платформу</b>  ''', reply_markup=keyboard, parse_mode='HTML')
 
-    bot.send_message(message.chat.id, '''<b>Приложение</b>  ''', reply_markup=keyboard,parse_mode='HTML')
 
 @bot.message_handler(commands=["phone"])
 def geophone(message):
