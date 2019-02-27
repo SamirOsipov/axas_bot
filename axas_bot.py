b52168ecde37e00745afa167ac83b04939988c0a
yoalexandra <alexandra.beznosova@gmail.com>
2019-02-27 11:02:55 +0300

    added callbacks

--- axas_bot.py
+++ axas_bot.py
@@ -11,14 +11,12 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def command_handler(message: Message):
-    # инлайнклавиатура для бота
-    keyboard = types.InlineKeyboardMarkup()
-    url_button = types.InlineKeyboardButton(text="Подробнее о нас", url="http://axas-soft.ru")
-    keyboard.add(url_button)
     # клавиатура для бота
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.row('Мобильные приложения', 'Веб-платформы')
     keyboard.row('Нейросети', 'Связаться с нами')
+    button_phone = types.KeyboardButton(text="Отправьте номер телефона, для связи с вами", request_contact=True)
+    keyboard.row(button_phone)
     bot.send_message(message.chat.id, '''<b>Добро пожаловать!</b> 
 Это телеграм бот компании AXAS. Что Вас интересует?''', reply_markup=keyboard, parse_mode='HTML')
 
@@ -26,7 +24,7 @@ def command_handler(message: Message):
 @bot.message_handler(content_types=['text'])
 def contacts_handler(message: Message):
     keyboard = types.InlineKeyboardMarkup()
-    if ('Связаться с нами' in message.text):
+    if 'Связаться с нами' in message.text:
         site_button = types.InlineKeyboardButton(text="Вебсайт", url="http://axas-soft.ru/")
         vk_button = types.InlineKeyboardButton(text="Вконтакте", url="https://vk.com/axas_soft")
         inst_button = types.InlineKeyboardButton(text="Инстаграм", url="http://axas-soft.ru/")
@@ -36,22 +34,21 @@ def contacts_handler(message: Message):
         keyboard.add(fb_button)
         keyboard.add(inst_button)
         bot.send_message(message.chat.id, "Hаши контакты.", reply_markup=keyboard)
-    elif ('Мобильные приложения' in message.text):
-        button_1 = types.InlineKeyboardButton(text='Приложение для бизнеса', url="http://axas-soft.ru")
-        button_2 = types.InlineKeyboardButton(text='Игры', url="http://axas-soft.ru")
+    elif 'Мобильные приложения' in message.text:
+        button_1 = types.InlineKeyboardButton(text='Приложение для бизнеса', callback_data='business_app')
+        button_2 = types.InlineKeyboardButton(text='Игры', callback_data='games')
         keyboard.add(button_1, button_2)
         bot.send_message(message.chat.id, 'Какую категорию приложения вы хотите заказать', reply_markup=keyboard)
-    elif ('Нейросети' in message.text):
+    elif 'Нейросети' in message.text:
         keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
         button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
         keyboard.row(button_phone)
         bot.send_message(message.chat.id, "Оставьте совой номер телефона!", reply_markup=keyboard)
-    elif ('Веб-платформы' in message.text):
+    elif 'Веб-платформы' in message.text:
         keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
         button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
         keyboard.row(button_phone)
         bot.send_message(message.chat.id, "Оставьте совой номер телефона!", reply_markup=keyboard)
-
 
 
 @bot.message_handler(content_types=['sticker'])
@@ -60,23 +57,24 @@ def sticker_handler(message: Message):
     bot.send_sticker(message.chat.id, STICKER_ID)
 
 
-
-@bot.message_handler(commands=['app'])
-def choose_buttom(message: Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    callback_button = types.KeyboardButton(text='Android-приложение')
-    keyboard.row(u'\uD83C\uDD95' + ' Android-приложение', u'\uD83D\uDC6B' + ' iOS-приложение')
-    keyboard.row('Обе платформы')
-    bot.send_message(message.chat.id, '''<b>Выберите платформу</b>  ''', reply_markup=keyboard, parse_mode='HTML')
-
-
-@bot.message_handler(commands=["phone"])
-def geophone(message):
-    # Эти параметры для клавиатуры необязательны, просто для удобства
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
-    keyboard.row(button_phone)
-    bot.send_message(message.chat.id, "Оставьте совой номер телефона!", reply_markup=keyboard)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    keyboard = types.InlineKeyboardMarkup()
+    # TODO: отправить данные на сервер - почта
+    if call.data == 'business_app':
+        ios_button = types.InlineKeyboardButton(text="iOS-приложение", callback_data='ios')
+        android_button = types.InlineKeyboardButton(text="Android-приложение", callback_data='android')
+        cross_button = types.InlineKeyboardButton(text="Обе платформы", callback_data='cross')
+        keyboard.add(ios_button)
+        keyboard.add(android_button)
+        keyboard.add(cross_button)
+        bot.send_message(call.message.chat.id, 'Выбрать платформу', reply_markup=keyboard)
+    elif call.data == 'cross':
+        ios_button = types.InlineKeyboardButton(text="Да", callback_data='yes')
+        android_button = types.InlineKeyboardButton(text="Нет", callback_data='no')
+        keyboard.add(ios_button)
+        keyboard.add(android_button)
+        bot.send_message(call.message.chat.id, 'Есть ли у Вас Тех задание, код, дизайн?', reply_markup=keyboard)
 
 
 bot.polling(timeout=60)
